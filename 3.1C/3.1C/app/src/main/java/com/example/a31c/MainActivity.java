e084728268c92a7cda66faec9979ab1b6f3a2dd0
Yee1955 <tyee11291129@gmail.com>
2024-03-20 14:32:15 +1100

    Upload Complete Functional App

--- 3.1C/3.1C/app/src/main/java/com/example/a31c/MainActivity.java
+++ 3.1C/3.1C/app/src/main/java/com/example/a31c/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.a31c;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        Button startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextName.getText().toString();
+                if (!name.matches("")) {
+                    User user = new User(name, 0);
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    intent.putExtra("USER_DATA", user);
+                    startActivity(intent);
+
+                    finish();
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Please Enter Your Name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
