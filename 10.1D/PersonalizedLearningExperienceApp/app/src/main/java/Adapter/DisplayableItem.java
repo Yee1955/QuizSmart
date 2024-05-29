49f512b19abee19bbda6152eca06f704789188d1
Yee1955 <tyee11291129@gmail.com>
2024-05-29 14:58:31 +1000

    First commit

--- 10.1D/PersonalizedLearningExperienceApp/app/src/main/java/Adapter/DisplayableItem.java
+++
@@ -1,8 +0,0 @@
-package Adapter;
-
-public interface DisplayableItem {
-    String getTitle();
-    String getDescription();
-    String getQuestion();
-    String[] getOptions();
-}
--- 5.1C/MyApplication/app/src/main/java/com/example/myapplication/onItemClickListener.java
+++
@@ -1,4 +0,0 @@
-package com.example.myapplication;
-public interface onItemClickListener {
-    void itemClick(News news);
-}
